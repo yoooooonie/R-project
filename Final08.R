4481e466ce968c7e7875d69128092eb6b93f2c20
Yoonmi Yang <ymyang@kookmin.ac.kr>
2022-06-09 16:31:47 +0900

    Final09

--- Final08.R
+++ Final08.R
@@ -93,5 +93,113 @@ fun7(1:50)
 fun7(1:50, med=TRUE)
 
 
-
 #8-8
+alice <- c('Alice', 'was', 'beginning', 'to','get','very','tired','of','sitting','by','her','sister','on','the','bank',
+           'and','of','having','nothing','to','do','once','or','twice','she','had','peeped','into','the','book',
+           'her','sister','was','reding','but','it','had','no','pictures','or','conversations','in','it','and','what',
+           'is','the','use','of','a','book','thought','Alice','without','pictures','or','conversations')
+count.letters <- function(x){
+  n.char <- nchar(x)
+  for(i in min(n.char):max(n.char)){
+    cat('These words have', i, 'letter(s) :\n')
+    print(toString(unique(alice[n.char==i])))
+  }
+}
+count.letters(alice)
+
+# to.count.letters <- function(x){
+#   n.char <- nchar(x)
+#   for(i in min(n.char):max(n.char)){
+#     cat('These words have', i, 'letter(s) :\n')
+#     print(unique(alice[n.char==i]))
+#   }
+# }
+# 
+# ne.count.letters(alice)
+# to.ne.count.letters(alice)
+# to.count.letters(alice)
+
+
+#8-9
+library(car)
+class(Salaries$salary)
+(a)
+mean.salary <- mean(Salaries$salary)
+mean.salary
+(b)
+sd.salary <- sd(Salaries$salary)
+sd.salary
+df9 <- Salaries$salary[(Salaries$salary>mean.salary-2*sd.salary) & (Salaries$salary<mean.salary+2*sd.salary)]
+head(df9)
+length(Salaries$salary)
+length(df9)
+mean(df9)
+(c)
+df9.3 <- Salaries$salary[(Salaries$salary>mean.salary-3*sd.salary) & (Salaries$salary<mean.salary+3*sd.salary)]
+head(df9.3)
+length(df9.3)
+mean(df9.3)
+
+
+cal.mean <- function(x, remove.outlier=FALSE, mult.outlier=2){
+  if(remove.outlier==TRUE){
+    outlier.index <- (x > mean(x, na.rm=TRUE) + mult.outlier * sd(x, na.rm = TRUE)) | 
+                     (x < mean(x, na.rm=TRUE) - mult.outlier * sd(x, na.rm = TRUE))
+    x.new <- x[!outlier.index]
+    result <- mean(x.new, na.rm=TRUE)
+  } else {
+    result <- mean(x, na.rm=TRUE)
+  }
+  return(result)
+}
+cal.mean(Salaries$salary)
+cal.mean(Salaries$salary, remove.outlier=TRUE)
+cal.mean(Salaries$salary, remove.outlier=TRUE, mult.outlier = 3)
+
+
+#8-10
+for (i in 1:12) {
+  cat('The month of',month.name[i],'\n')
+}
+
+#8-11
+vector11 <- c('Superman','Batman','Spiderman')
+for (i in vector11){
+  cat('Hello',i,'\b...\n')
+}
+for (i in vector11){
+  cat(sprintf('Hello %s...\n',i))
+}
+
+
+#8-12
+fun12 <- function(x) sample(c('H',"T"), size=1)
+nflip <- 0
+nhead <- 0
+repeat{
+  if(nhead>=10) break
+  else {
+    if(fun12()=='H') {
+      nhead <- nhead+1
+    } else nhead <- 0
+  }
+  nflip <- nflip + 1
+}
+nflip
+
+
+#8-13
+fun13 <- function() paste(sample(LETTERS, size = 3, replace=TRUE), collapse = '')
+
+nflip13 <- 0
+df13 <- data.frame(nflip13=numeric(), result=character())
+flag <- TRUE
+while(flag){
+  nflip13 <- nflip13 + 1
+  result <- fun13()
+  rbind(df13, nflip13=nflip13, result=result)
+  cat(nflip13,':', result,'\n')
+  if(result=='ABC') flag <- FALSE
+}
+nflip13
+
---
+++ Final09.R
@@ -0,0 +1,103 @@
+#Chapter09 데이터 전처리
+#9-1
+x <- 5:10
+x[c(1,2)] <- 2:3
+x
+x[-1]
+x[-1] <- 1:5
+x
+
+#9-2
+mtcars$weight[mtcars$wt > median(mtcars$wt)] <-'heavy'
+mtcars$weight[mtcars$wt <= median(mtcars$wt)] <-'light'
+head(mtcars)
+tail(mtcars)
+
+
+#9-3
+mtcars[,order(colnames(mtcars))]
+
+
+#9-4
+#mtcars[mtcars$cyl=4,]
+mtcars[mtcars$cyl==4,]
+mtcars[-1:4,] #Incorrect
+#mtcars[mtcars$cyl<=5]
+mtcars[mtcars$cyl<=5,]
+#mtcars[mtcars$cyl==4|6, ]
+mtcars[mtcars$cyl==4|mtcars$cyl==6, ]
+mtcars[mtcars$cyl %in% c(4,6), ]
+
+
+#9-5
+mtcars[1:15]
+mtcars[1:15,]
+
+
+#9-6
+head(iris)
+sample.index <- sample(1:nrow(iris), size=nrow(iris)*0.7)
+iris[sample.index,]
+
+library(dplyr)
+slice_sample(iris, prop=0.7)
+
+
+
+#9-7
+names <- c('Gates','Jobs',"Bezos")
+lapply(names, nchar)
+sapply(names, nchar)
+
+
+#9-8
+lst1 <- list(A=matrix(1:12, nrow=3),B=matrix(1:12, nrow=2), C=1:12)
+lst2 <- list(A=matrix(1:12, nrow=3),B=matrix(1:12, nrow=6), C=12:1)
+identical(lst1, lst2)
+mapply(identical, lst1, lst2)
+
+
+#9-9
+head(state.x77)
+apply(state.x77, 2, mean)
+apply(state.x77, 2, sd)
+
+
+#9-10
+install.packages('nycflights13')
+library(nycflights13)
+head(flights)
+str(flights)
+library(dplyr)
+
+table(flights)
+
+aggregate(flights['dest'], list(Group.dest=flights$dest), 
+          FUN=count)
+
+
+df10 <- flights['dest','count','dist','delay']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
